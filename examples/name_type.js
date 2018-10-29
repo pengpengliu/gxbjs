b7457e20fd7fb773829c8350a0024cff9730bf27
lanhaoxiang <lanhaoxiang@qq.com>
2018-10-29 13:11:08 +0800

    feat: export object_id_type method

--- examples/name_type.js
+++ examples/name_type.js
@@ -1,50 +1,8 @@
-import v from '../lib/serializer/src/SerializerValidation';
-
-const char_to_symbol = (c) => {
-    let result = 0;
-    if (c >= 'a'.charCodeAt(0) && c <= 'z'.charCodeAt(0))
-        result = (c - 'a'.charCodeAt(0)) + 6;
-    if (c >= '1'.charCodeAt(0) && c <= '5'.charCodeAt(0))
-        result = (c - '1'.charCodeAt(0)) + 1;
-    return result;
-};
-
-const string_to_name = (str) => {
-    var name = v.to_long(0).toUnsigned();
-    var i = 0;
-    for (; str[i] && i < 12; ++i) {
-        // NOTE: char_to_symbol() returns char type, and without this explicit
-        // expansion to uint64 type, the compilation fails at the point of usage
-        // of string_to_name(), where the usage requires constant (compile time) expression.
-        let symbol = v.to_long(char_to_symbol(str[i].charCodeAt(0))).toUnsigned().and(0x1f);
-        name = name.or(symbol.shiftLeft(64 - 5 * (i + 1)));
-    }
-
-    // The for-loop encoded up to 60 high bits into uint64 'name' variable,
-    // if (strlen(str) > 12) then encode str[12] into the low (remaining)
-    // 4 bits of 'name'
-    if (i == 12) {
-        let symbol = v.to_long(char_to_symbol(str[12].charCodeAt(0))).toUnsigned();
-        name = name.or(symbol.and(0x0F));
-    }
-    return name;
-};
-
-const name_to_string = (name) => {
-    let charmap = ".12345abcdefghijklmnopqrstuvwxyz";
-    let str = [];
-    let tmp = name;
-    for (var i = 0; i <= 12; ++i) {
-        let index = tmp.and(i == 0 ? 0x0f : 0x1f).toInt();
-        let c = charmap[index];
-        str.push(c);
-        tmp = tmp.shiftRightUnsigned(i == 0 ? 4 : 5);
-    }
-    return str.reverse().join('').replace(/\.+$/g, '');
-};
+import {name_to_string, object_id_type, string_to_name} from "../lib/serializer";
 
 console.log(string_to_name('subbalance').toString());
 console.log(name_to_string(string_to_name('subbalance')));
+console.log(object_id_type("1.2.576").toString());
 
 
 
