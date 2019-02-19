063cecf35d4f9e178b3dfc902c0bbfafa1fbf35d
lanhaoxiang <lanhaoxiang@qq.com>
2019-02-19 16:49:34 +0800

    chore: update dist file

--- dist/tx_serializer.js
+++ dist/tx_serializer.js
@@ -19,6 +19,8 @@ var _bytebuffer = require("bytebuffer");
 
 var _bytebuffer2 = _interopRequireDefault(_bytebuffer);
 
+require("babel-polyfill");
+
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
