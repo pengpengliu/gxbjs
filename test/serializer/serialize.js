f09e93fdbb74feff2766465064b8e6409fe875ae
刘宗源 <940101379@qq.com>
2019-02-27 16:10:05 +0800

    refactor: serializeCallData use makeSerializer

--- test/serializer/serialize.js
+++ test/serializer/serialize.js
@@ -1,12 +1,15 @@
 import assert from "assert";
-import {serializeCallData, deserializeCallData} from "../../lib/tx_serializer.js";
-import {bank_abi} from "./static/abi.js";
+import { serializeCallData, deserializeCallData } from "../../lib/tx_serializer.js";
+import { bank_abi } from "./static/abi.js";
 
-describe("operation test", function() {
-    it("serialize", function(){
-        console.log("dessss", serializeCallData(bank_abi, "withdraw",));
+describe("operation test", function () {
+    it("serialize", function () {
+        console.log("seeeeesss", serializeCallData("withdraw", {
+            to_account: "youxiu123",
+            amount: { amount: "100000", asset_id: "1" }
+        }, bank_abi));
     });
-    it("deserialize", function(){
-        console.log("dessss", deserializeCallData(bank_abi, "withdraw","09796f75786975313233a0860100000000000100000000000000"));
+    it("deserialize", function () {
+        console.log("dessss", deserializeCallData("withdraw", "09796f75786975313233a0860100000000000100000000000000", bank_abi));
     });
 });
