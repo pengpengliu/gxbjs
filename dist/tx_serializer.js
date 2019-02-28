e82e50d9d93f4072c87f8caa182664b86da42354
刘宗源 <940101379@qq.com>
2019-02-28 16:42:57 +0800

    chore: remove babel polyfill to babel runtime

--- dist/tx_serializer.js
+++ dist/tx_serializer.js
@@ -20,8 +20,6 @@ var ops = _interopRequireWildcard(_operations);
 
 var _serializer = require("./serializer");
 
-require("idempotent-babel-polyfill");
-
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
