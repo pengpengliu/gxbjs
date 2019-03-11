44d9af4358c53a5cf21f725b418d186862e4828f
刘宗源 <940101379@qq.com>
2019-03-11 15:07:51 +0800

    fix: string chinese characters bug

---
+++ examples/brainKey.js
@@ -0,0 +1,5 @@
+import {
+    key
+} from "../lib";
+
+console.log(key.get_brainPrivateKey("sticks bilious nonform piqure linaga jejune cellist dural golden cidaris subaud juniata nailing outjut balloon sasa").toWif());
--- test/serializer/serialize.js
+++ test/serializer/serialize.js
@@ -1,6 +1,6 @@
 import assert from "assert";
 import { serializeCallData, deserializeCallData } from "../../lib/tx_serializer.js";
-import { bank_abi } from "./static/abi.js";
+import { bank_abi, hello_abi } from "./static/abi.js";
 
 describe("operation test", function () {
     it("serialize", function () {
@@ -11,5 +11,10 @@ describe("operation test", function () {
     });
     it("deserialize", function () {
         console.log("dessss", deserializeCallData("withdraw", "09796f75786975313233a0860100000000000100000000000000", bank_abi));
+    });
+    it("serialize", function () {
+        console.log("seeeeesss", serializeCallData("hi", {
+            user: "刘宗源"
+        }, hello_abi));
     });
 });
